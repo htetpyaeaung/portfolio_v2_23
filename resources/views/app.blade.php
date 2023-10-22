d0eb00b8485913b8ece8edbffbfe24037232c3ce
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2023-10-22 19:09:26 +1030

    Initializing the portfolio v2 project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class="md:container md:mx-auto">
+    <h1>Hello</h1>
+  </div>
+</body>
+</html>
