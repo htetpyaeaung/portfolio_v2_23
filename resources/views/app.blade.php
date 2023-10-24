75d83960805738fc096ff56b82b537d1be42f47e
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2023-10-25 01:25:32 +1030

    Add center container for name and icon container to landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
-  <div class="md:container md:mx-auto">
-    <h1>Hello</h1>
+<body class="font-inter">
+  <div class="bg-cover bg-stone-300 w-full h-screen flex">
+    <div class="flex-none">
+      <h1>Icon Here</h1>
+    </div>
+    <div class="flex-auto w-100">
+      <div class="container mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 class="font-bold sm:text-base md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl "><span class=" p-8 border-4 border-zinc-950">Big Name</span></h1>
+      </div>
+    </div>
+  </div>
+  <div class="container w-full">
+    <h1>Another Container</h1>
   </div>
 </body>
-</html>
+</html
