748fa996605cba5eccbe5cd6e6f34c0da9370578
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-03-03 19:10:02 +1030

    Add photos for each project item

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -8,6 +8,28 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Chela+One&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+  <style>
+      @foreach ($projectItems as $item)
+      #{{ $item['id'] }}:before {
+            /* background-image: url('{{ asset('storage/images/'.$item['image']) }}'); */
+            background: white url('{{ asset('storage/images/'.$item['image']) }}') no-repeat center center / contain;
+
+            content: "";
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            /* background-repeat: no-repeat; */
+            /* background-size: cover; */
+            filter: brightness(60%);
+            z-index: 1;
+        }
+      @endforeach
+      #cara-integration:before{
+        background-size:contain;
+      }
+  </style>
   @vite('resources/css/app.css')
   @vite('resources/js/app.js')
 </head>
@@ -58,18 +80,40 @@
     <h1 class="xxs:w-full xs:w-3/4 md:w-72 lg:w-2/5 xl:w-80 font-scoville relative section-headers projectHead text-4xl" id="sectionHeader">Featured Projects</h1>
   </div>
 
-  <div class="flex mx-auto xxs:w-11/12 lg:w-10/12 p-4 text-center xxs:justify-center items-center lg:justify-end bg-white shadow-2xl mt-9 project-item" id="firstPJ">
-    <div class="flex items-center text-center flex-col xxs:justify-end lg:justify-center xxs:w-3/4 xxs:h-4/5 lg:w-2/5 lg:h-2/4">
-        <h2 class="xxs:text-2xl lg:text-3xl font-extrabold">Middleware at Cara</h2>
-        <p>Transactional Middleware</p>
-        <div class="w-full h-auto mx-auto flex items-center justify-center">
-          <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">Red</span>
-          <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">Red</span>
+  @foreach ($projectItems as $item)
+    <div class="flex mx-auto xxs:w-11/12 lg:w-10/12 p-4 text-center xxs:justify-center items-center lg:justify-end bg-white shadow-2xl mt-9 project-item" id="{{ $item['id'] }}">
+      <div class="flex text-white backdrop-blur-sm items-center z-10 text-center flex-col xxs:justify-end lg:justify-center xxs:w-3/4 xxs:h-4/5 lg:w-2/5 lg:h-2/4">
+        <h2 class="xxs:text-2xl lg:text-3xl font-extrabold">{{ $item['title'] }}</h2>
+        <p>{{ $item['description'] }}</p>
+        <div class="w-full h-auto mx-auto flex items-center justify-center mt-2">
+          @foreach ($item['tags'] as $tag)
+            <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">{{ $tag }}</span>
+          @endforeach
         </div>
+        <br/>
+        <a href='{{ '/'.$item['id'] }}' target="_blank">
+          <button class="relative mt-2 z-10 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Read Case Study
+            </span>
+          </button>
+        </a>
+      </div>
     </div>
+  @endforeach
+
+  <div class="flex mx-auto w-10/12 p-4 text-center justify-center items-center mt-9">
+    <a href="https://github.com/htetpyaeaung" target="_blank">
+      <button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
+        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+        </svg>
+        See more on Github
+        </button>
+    </a>
+    
   </div>
-
-
+  
 </div>
   
 
@@ -77,7 +121,7 @@
   <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">@php
     echo '&copy; '.date('Y');
   @endphp
-   Crafted with &#10084; by Htet Pyae Aung
+   Crafted with &#10084; by Htet Pyae Aung | Resevoir Photo by <a href="https://unsplash.com/@vladizlo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vlad Zaytsev</a> on <a href="https://unsplash.com/photos/boat-on-lake-near-green-trees-during-daytime-ipKxS3liiiA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
   </span>
   <ul class="flex flex-wrap items-center mt-3 text-sm gap-3  text-gray-500 dark:text-gray-400 sm:mt-0">
       <li>
