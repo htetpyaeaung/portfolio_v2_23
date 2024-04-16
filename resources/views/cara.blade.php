20cf078571c667a599011277fc202618b54c5fa3
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-04-16 22:02:29 +0930

    Modify Cara Blade View

--- resources/views/cara.blade.php
+++ resources/views/cara.blade.php
@@ -54,12 +54,12 @@
     
     <section class="w-full h-full flex flex-col xxs:mt-24 lg:mt-28" id='project_section'>
         <div class="flex flex-col mx-auto w-full text-center justify-center items-center" id="project_hero" style="height: 50vh;">
-            <h1 class="font-scoville text-7xl" id="sectionHeader">Cara Middleware</h1>
-            <h2 class="text-md font-normal">A scalable cloud-based custom integration middleware</h2>
+            <h1 class="font-scoville xxs:text-4xl md:text-7xl" id="sectionHeader">Cara Middleware</h1>
+            <h2 class="xxs:text-sm md:text-md font-normal">A scalable cloud-based custom integration middleware</h2>
         </div>
-        <div class="flex mx-auto w-full h-1/2 text-center justify-center items-center" id="project_hero">
-            <img class="px-12 py-12 w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
-            <img class="px-12 py-12 w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
+        <div class="flex flex-col md:flex-row mx-auto w-full h-1/2 text-center justify-center items-center" id="project_hero">
+            <img class="px-2 py-2 w-full md:w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
+            <img class="px-2 py-2 w-full md:w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
         </div>
 
         <div class="flex xxs:flex-wrap md:flex-nowrap mx-auto w-full h-full text-center justify-center items-center xxs:space-y-2 md:space-y-0 xxs:space-x-1 md:space-x-2" id="project_hero">
@@ -80,34 +80,92 @@
             </div>
         </div>
 
-        <div class="flex flex-col mx-auto w-full text-center bg-white">
-            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 text-center justify-center items-start" id="project_hero" style="height: 40vh;">
-                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto text-center justify-left items-start">
-                    <h1 class="font-scoville text-5xl" id="sectionHeader">Background</h1>
-                    <p class="mt-2">Cara is a leading NDIS disability service provider working alongside people with disability across metropolitan Adelaide and regional South Australia. <a href="https://www.cara.org.au" class="underline">Learn More about what CARA does.</a></p><br>
-                    <p>The goal of this project to identify the potential integration points between the critical business systems use across the business and to drastically reduce manual work by Staff.</p>
+        <div class="flex flex-col mx-auto w-full bg-white">
+            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 md:my-5 h-full justify-center items-start" id="project_hero">
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h1 class="font-scoville text-4xl" id="sectionHeader">Background</h1>
+                    <p class="mt-2">Cara is a leading NDIS disability service provider working alongside people with disability across metropolitan Adelaide and regional South Australia. <a href="https://www.cara.org.au" class="underline">Learn More about what CARA does.</a></p>
+                    <p class="my-5">The goal of this project to identify the potential integration points between the critical business systems use across the business and to drastically reduce manual work by Staff.</p>
                 </div>
             </div>
         </div>
         <div class="flex flex-col mx-auto w-full bg-orange-950 text-white">
-            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 justify-center items-start" id="project_hero" style="height: 40vh;">
-                {{-- <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-center items-start">
-                    <h1 class="font-scoville text-5xl" id="sectionHeader">Problem</h1>
-                    <p class="mt-2">Before the integration system, the employee onboarding and offboarding data flow take almost 30 minutes for HR department. 
-                        HR staff has to manually enter data across different systems such as rostering, HR and ERP systems and also has to ensure data are consistence and correct.</p>
-                </div> --}}
+            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_hero">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h1 class="font-scoville text-5xl" id="sectionHeader">Problem</h1>
-                    <p class="mt-2">Before the integration system, the employee onboarding and offboarding data flow take almost 30 minutes for HR department. 
-                        HR staff has to manually enter data across rostering, HR and ERP systems and also has to ensure data are consistence and correct.</p>
+                    <h1 class="font-scoville text-4xl" id="sectionHeader">Problem</h1>
+                    <blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-orange-950 dark:border-gray-500 dark:bg-gray-800">
+                        <p class="text-xl italic font-bold leading-relaxed text-white">"15 Minutes"</p>
+                    </blockquote>
+                    <p class="text-white">
+                    That is how long it takes for a HR staff to onboard or offboard an employee. HR manages a massive information of staff and it takes a significant time for them to manually enter or update the information across the different systems.<br/><br/>
+                    Data are scattered across and there are multiple source of truth for the similar data type and the risk of data duplication in various areas. <br/><br/>
+                    Cara need to streamline this processes to reduce this time-consuming, repetitive task and efficiently manage their scattered data. <br/><br/>
+                    </p>
                 </div>
             </div>
         </div>
-        <div class="flex flex-col mx-auto w-full text-center  bg-yellow-600 text-white">
-            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 text-center justify-center items-start" id="project_hero" style="height: 60vh;">
-                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto text-center justify-left items-start">
-                    <h1 class="font-scoville text-5xl" id="sectionHeader">Solution</h1>
-                    <p class="mt-2">A Custom Integration System, designed to minimize manual data entry, enhance operational efficiency, and ensure consistent data across all SaaS platforms utilized by CARA.</p>
+        <div class="flex flex-col mx-auto w-full bg-yellow-600 text-white">
+            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_hero">
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h1 class="font-scoville text-4xl" id="sectionHeader">Solution</h1>
+                    <blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-yellow-600 dark:border-gray-500 dark:bg-gray-800">
+                        <p class="xxs:text-sm md:text-xl italic font-bold leading-relaxed text-white">"Establish a source of truth for all staff information and integrate different systems through centralized integration hub"</p>
+                    </blockquote>
+                    <p class="my-4">
+                        The goal is to design and develop a scalable customized middleware system that allows the data flows from source of truth to all other systems.<br/><br/>
+                        Staff will make changes to one and only one system and the data will be replicated across all different systems that are connected to the middleware. <br/><br/>
+                        This ensures data consistency, reduce the workload and increase efficiency of the staff.<br/></p>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col mx-auto w-full bg-indigo-900 text-white">
+            <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_process">
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h1 class="font-scoville text-4xl" id="sectionHeader">Process</h1>
+                    <p class="my-4 md:leading-10">
+                        Cara hold staff information in both HR Information system and ERP system. Staff have direct access to update their details in ERP system and only HR staff are allowed to enter or update the records in HR Information system. Since ERP allows individual modifications of the profiles, some details are not copied over to HR system and some have been outdated like phone numbers or the address. To avoid data duplication and enhance data consistencies, access to update the profile is restricted and data should only be updated in HR system.<br/>
+                    </p>
+                </div>
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h2 class="text-2xl md:text-3xl" > How do we ensure data in HR system is up to date?</h2>
+                    <p class="my-4 md:leading-10">
+                        With the amount of data hold in the system, it is impossible to go through individual profiles in two different systems and match up each other.<br/>
+                    <ol class="list-decimal ml-4">
+                        <li>Staff are notified about the existing personal details hold in the HR system and asking them to update if any of them are changed by the specific due date.</li>
+                        <li>By the due date, the details will be used to update the HR system to have the latest information.</li>
+                    </ol><br/>
+                    Instead of checking two different systems to find the unmatched information, HR staff only need to focus on one system and the replies from the staff.
+                    </p><br/>
+                </div>
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Requirement Gathering: Is the project feasible?</h2>
+                    <p class="my-4 md:leading-10">
+                        Before we start doing this integrations, we need to gather data on whether the systems are even connectable.<br/>
+                        This required two phases: <br/>
+                    <ol class="list-decimal ml-4">
+                        <li>We look at the individual systems, meaning diving through their documentation and check whether they have public APIs since we will be using that to integrate through centralized hub.</li>
+                        <li>If no, then we have to look at alternatives on how we can integrate differently for that system. </li>
+                    </ol><br/>
+                    Fortunately, most of the systems at Cara support public APIs so the project is feasible to proceed.
+                    </p><br/>
+                </div>
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Build or Buy: Middleware</h2>
+                    <p class="my-4 md:leading-10">
+                        Now that the project is feasible and research is being done on the projected efficiecy of the system, we need to decide on buying a COTS or build a customized solution tailored to the business. A lot of factors are influenced in making a decision on this. However, senior management decided to proceed with building a customized solution rather than buying a COTS, we went ahead with that.
+                    </p><br/>
+                </div>
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Roadblocks</h2>
+                    <p class="my-4 md:leading-10">
+                        Some legacy systems do not support the modern REST API methods which is the case for some systems at CARA. Like all the other API endpoints, there is a rate limit imposed on APIs so we need to ensure the system runs efficiently and effectively without hitting the limit for normal tasks.
+                    </p><br/>
+                </div>
+                <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Results</h2>
+                    <p class="my-4 md:leading-10">
+                        A scalable customized middleware that digests the data flow from source of truth and spit out the tailored processed data back to the connected system. 
+                    </p><br/>
                 </div>
             </div>
         </div>
