80702457179097e3ee21ad8b24ed45110af11d7e
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-04-17 21:16:22 +0930

    Modify Cara Blade View

--- resources/views/cara.blade.php
+++ resources/views/cara.blade.php
@@ -89,14 +89,14 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-col mx-auto w-full bg-orange-950 text-white">
+        <div class="flex flex-col mx-auto w-full bg-white text-black">
             <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_hero">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Problem</h1>
-                    <blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-orange-950 dark:border-gray-500 dark:bg-gray-800">
-                        <p class="text-xl italic font-bold leading-relaxed text-white">"15 Minutes"</p>
+                    <blockquote class="p-4 my-4 border-s-4 border-gray-300  dark:border-gray-500 dark:bg-gray-800">
+                        <p class="text-xl italic font-bold leading-relaxed ">"15 Minutes"</p>
                     </blockquote>
-                    <p class="text-white">
+                    <p>
                     That is how long it takes for a HR staff to onboard or offboard an employee. HR manages a massive information of staff and it takes a significant time for them to manually enter or update the information across the different systems.<br/><br/>
                     Data are scattered across and there are multiple source of truth for the similar data type and the risk of data duplication in various areas. <br/><br/>
                     Cara need to streamline this processes to reduce this time-consuming, repetitive task and efficiently manage their scattered data. <br/><br/>
@@ -104,12 +104,12 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-col mx-auto w-full bg-yellow-600 text-white">
+        <div class="flex flex-col mx-auto w-full bg-white text-black">
             <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_hero">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Solution</h1>
-                    <blockquote class="p-4 my-4 border-s-4 border-gray-300 bg-yellow-600 dark:border-gray-500 dark:bg-gray-800">
-                        <p class="xxs:text-sm md:text-xl italic font-bold leading-relaxed text-white">"Establish a source of truth for all staff information and integrate different systems through centralized integration hub"</p>
+                    <blockquote class="p-4 my-4 border-s-4 border-gray-300  dark:border-gray-500 dark:bg-gray-800">
+                        <p class="xxs:text-sm md:text-xl italic font-bold leading-relaxed">"Establish a source of truth for all staff information and integrate different systems through centralized integration hub"</p>
                     </blockquote>
                     <p class="my-4">
                         The goal is to design and develop a scalable customized middleware system that allows the data flows from source of truth to all other systems.<br/><br/>
@@ -118,16 +118,16 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-col mx-auto w-full bg-indigo-900 text-white">
+        <div class="flex flex-col mx-auto w-full bg-white text-black">
             <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_process">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h1 class="font-scoville text-4xl" id="sectionHeader">Process</h1>
+                    <h1 class="font-scoville text-4xl " id="sectionHeader">Process</h1>
                     <p class="my-4 md:leading-10">
                         Cara hold staff information in both HR Information system and ERP system. Staff have direct access to update their details in ERP system and only HR staff are allowed to enter or update the records in HR Information system. Since ERP allows individual modifications of the profiles, some details are not copied over to HR system and some have been outdated like phone numbers or the address. To avoid data duplication and enhance data consistencies, access to update the profile is restricted and data should only be updated in HR system.<br/>
                     </p>
                 </div>
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h2 class="text-2xl md:text-3xl" > How do we ensure data in HR system is up to date?</h2>
+                    <h2 class="text-2xl md:text-3xl font-bold" > How do we ensure data in HR system is up to date?</h2>
                     <p class="my-4 md:leading-10">
                         With the amount of data hold in the system, it is impossible to go through individual profiles in two different systems and match up each other.<br/>
                     <ol class="list-decimal ml-4">
@@ -138,7 +138,7 @@
                     </p><br/>
                 </div>
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Requirement Gathering: Is the project feasible?</h2>
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl font-bold" > Requirement Gathering: Is the project feasible?</h2>
                     <p class="my-4 md:leading-10">
                         Before we start doing this integrations, we need to gather data on whether the systems are even connectable.<br/>
                         This required two phases: <br/>
@@ -150,57 +150,43 @@
                     </p><br/>
                 </div>
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Build or Buy: Middleware</h2>
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl font-bold" > Build or Buy: Middleware</h2>
                     <p class="my-4 md:leading-10">
                         Now that the project is feasible and research is being done on the projected efficiecy of the system, we need to decide on buying a COTS or build a customized solution tailored to the business. A lot of factors are influenced in making a decision on this. However, senior management decided to proceed with building a customized solution rather than buying a COTS, we went ahead with that.
                     </p><br/>
                 </div>
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Roadblocks</h2>
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl font-bold" > Roadblocks</h2>
                     <p class="my-4 md:leading-10">
                         Some legacy systems do not support the modern REST API methods which is the case for some systems at CARA. Like all the other API endpoints, there is a rate limit imposed on APIs so we need to ensure the system runs efficiently and effectively without hitting the limit for normal tasks.
                     </p><br/>
                 </div>
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
-                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl" > Results</h2>
+                    <h2 class="text-2xl md:text-3xltext-2xl md:text-3xl font-bold" > Results</h2>
                     <p class="my-4 md:leading-10">
                         A scalable customized middleware that digests the data flow from source of truth and spit out the tailored processed data back to the connected system. 
                     </p><br/>
                 </div>
+                <div class="flex flex-col md:flex-row mx-auto px-7 pt-8 w-full h-auto justify-between items-start">
+                    <button type="button" class="text-white bg-rose-900 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0l4 4m-4-4l4-4"/>
+                        </svg>
+                        &nbsp; Prev: SA Water Resevoir Data Analysis                     
+                    </button>
+                    <button type="button" class="text-white bg-rose-900 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Next: New System at Thermo Fisher
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </button>
+                </div> <br/><br/>
+                
             </div>
         </div>
     </section>
     </div>
-    <footer class="bottom-0 left-0 z-20 w-full p-4 font-bold bg-stone-300 md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">@php
-        echo '&copy; 2024', ($year = gmdate("Y")) !== '2024'? ' - '.$year : '';
-    @endphp
-    Crafted with &#10084; by Htet Pyae Aung
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm gap-3 footer-icons text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="https://www.twitter.com" target="_blank">
-            <svg class="w-7 h-7 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M13.8 10.5 20.7 2h-3l-5.3 6.5L7.7 2H1l7.8 11-7.3 9h3l5.7-7 5.1 7H22l-8.2-11.5Zm-2.4 3-1.4-2-5.6-7.9h2.3l4.5 6.3 1.4 2 6 8.5h-2.3l-4.9-7Z"/>
-            </svg>
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/htetpyaeaung" target="_blank">
-            <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M12 2c-2.4 0-4.7.9-6.5 2.4a10.5 10.5 0 0 0-2 13.1A10 10 0 0 0 8.7 22c.5 0 .7-.2.7-.5v-2c-2.8.7-3.4-1.1-3.4-1.1-.1-.6-.5-1.2-1-1.5-1-.7 0-.7 0-.7a2 2 0 0 1 1.5 1.1 2.2 2.2 0 0 0 1.3 1 2 2 0 0 0 1.6-.1c0-.6.3-1 .7-1.4-2.2-.3-4.6-1.2-4.6-5 0-1.1.4-2 1-2.8a4 4 0 0 1 .2-2.7s.8-.3 2.7 1c1.6-.5 3.4-.5 5 0 2-1.3 2.8-1 2.8-1 .3.8.4 1.8 0 2.7a4 4 0 0 1 1 2.7c0 4-2.3 4.8-4.5 5a2.5 2.5 0 0 1 .7 2v2.8c0 .3.2.6.7.5a10 10 0 0 0 5.4-4.4 10.5 10.5 0 0 0-2.1-13.2A9.8 9.8 0 0 0 12 2Z" clip-rule="evenodd"/>
-            </svg>    
-            </a>  
-        </li>
-        <li>
-            <a href="https://www.linkedin.com/in/htetpyaeaung-charles/" target="_blank">
-            <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
-                <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
-            </svg>
-            </a> 
-        </li>
-    </ul>
-    </footer>
+    
+
 </body>
 </html
