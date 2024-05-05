f5cb133efab35d8af30eaf17804f4f070229092f
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-05-05 21:12:11 +0930

    Add language to html block in Thermo Fisher page

--- resources/views/thermo.blade.php
+++ resources/views/thermo.blade.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html class="scroll-smooth">
+<html class="scroll-smooth" lang="en-US">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="{{ asset('storage/images/favicon.ico') }}">
-  <title>Cara Integration | Projects</title>
+  <title>Projects | Thermo Fisher</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Chela+One&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
@@ -47,29 +47,28 @@
     
     <section class="w-full h-full flex flex-col xxs:mt-24 lg:mt-28" id='project_section'>
         <div class="flex flex-col mx-auto w-full text-center justify-center items-center" id="project_hero" style="height: 50vh;">
-            <h1 class="font-scoville xxs:text-4xl md:text-7xl" id="sectionHeader">Cara Middleware</h1>
-            <h2 class="xxs:text-sm md:text-md font-normal">A scalable cloud-based custom integration middleware</h2>
+            <h1 class="font-scoville xxs:text-4xl md:text-7xl" id="sectionHeader">Maintenance Information System</h1>
+            <h2 class="xxs:text-sm md:text-md font-normal">An accessible, centralized maintenance data managmeent system tailored towards the requirements of Thermo Fisher Scientific</h2>
         </div>
         <div class="flex flex-col md:flex-row mx-auto w-full h-1/2 text-center justify-center items-center" id="project_hero">
-            <img class="px-2 py-2 w-full md:w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
-            <img class="px-2 py-2 w-full md:w-1/2 h-1/2" src="{{ asset('storage/images/cara_mw.png') }}">
+            <img class="px-2 py-2 w-full md:w-1/2 h-1/2" src="{{ asset('storage/images/thermo-fisher-portal.png') }}">
         </div>
 
         <div class="flex xxs:flex-wrap md:flex-nowrap mx-auto w-full h-full text-center justify-center items-center xxs:space-y-2 md:space-y-0 xxs:space-x-1 md:space-x-2" id="project_hero">
             <div class="flex flex-col w-full h-1/2 justify-center py-12">
                 <h2 class="font-bold text-2xl py-3">Role</h2>
-                <h2 class="text-md py-3">Application Developer</h2>
-                <h3 class="text-sm">Custom Integration, Testing, APIs, Salesforce, Laravel, MariaDB, Redis, AWS</h3>
+                <h2 class="text-md py-3">Analyst Developer</h2>
+                <h3 class="text-sm">Custom Integration, Testing, APIs, WordPress, Php</h3>
             </div>
             <div class="flex flex-col justify-center w-full h-1/2 py-12">
                 <h2 class="font-bold text-2xl py-3">Team</h2>
-                <h2 class="text-md py-3">Development team, Business Analysts & Project Manager</h2>
-                <h3 class="text-sm">Managed by Head of IT</h3>
+                <h2 class="text-md py-3">Development team, Clients & Project Manager</h2>
+                <h3 class="text-sm">Managed by Production Manager, Thermo Fisher Adelaide</h3>
             </div>
             <div class="flex flex-col justify-center text-center w-full h-1/2 py-12">
                 <h2 class="font-bold text-2xl py-3">Duration</h2>
-                <h2 class="text-md py-3">February - September 2023</h2>
-                <h2 class="text-sm">(8 months)</h2>
+                <h2 class="text-md py-3">August - December 2021</h2>
+                <h2 class="text-sm">(4 months)</h2>
             </div>
         </div>
 
@@ -77,8 +76,9 @@
             <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 md:my-5 h-full justify-center items-start" id="project_hero">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Background</h1>
-                    <p class="mt-2">Cara is a leading NDIS disability service provider working alongside people with disability across metropolitan Adelaide and regional South Australia. <a href="https://www.cara.org.au" class="underline">Learn More about what CARA does.</a></p>
-                    <p class="my-5">The goal of this project to identify the potential integration points between the critical business systems use across the business and to drastically reduce manual work by Staff.</p>
+                    <p class="mt-2">Thermo Fisher Scientific is a leading global supplier of analytical instruments, life sciences solutions, specialty diagnostics, laboratory, pharmaceutical and biotechnology services. <a href="https://www.thermofisher.com" class="underline">Learn More about what Thermo Fisher does.</a></p>
+                    <p class="my-5">This project was done in a collaboration with Thermo Fisher's Australian Division with the goal of improving efficiencies and modernizing the business. The aim of the project 
+                         is to improve their machine maintenance process by digitalizing and creating a web portal for storing current and archived maintenance forms.</p>
                 </div>
             </div>
         </div>
@@ -87,12 +87,11 @@
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Problem</h1>
                     <blockquote class="p-4 my-4 border-s-4 border-gray-300  dark:border-gray-500 dark:bg-gray-800">
-                        <p class="text-xl italic font-bold leading-relaxed ">"15 Minutes"</p>
+                        <p class="text-xl italic font-bold leading-relaxed ">"Maintenance Manager has to dive through a stack of folders to find archived forms and staff has to keep track of multiple paper forms for individual machines manually"</p>
                     </blockquote>
                     <p>
-                    That is how long it takes for a HR staff to onboard or offboard an employee. HR manages a massive information of staff and it takes a significant time for them to manually enter or update the information across the different systems.<br/><br/>
-                    Data are scattered across and there are multiple source of truth for the similar data type and the risk of data duplication in various areas. <br/><br/>
-                    Cara need to streamline this processes to reduce this time-consuming, repetitive task and efficiently manage their scattered data. <br/><br/>
+                        Currently, the maintenance department use paper forms and the supervisor has to print out and distribute to other employees manually to get the work update.
+                        This becomes extra steps for every maintenace process as staff has to fill in multiple paper forms manually and submit them to the manager. It becomes a risk to the business as the paper forms and data could be lost, confidential data can be outside of business and no real-time tracking for management to see which machine is behind in the maintenance process.
                     </p>
                 </div>
             </div>
@@ -101,13 +100,20 @@
             <div class="flex flex-col mx-auto xxs:w-full md:w-3/4 h-full justify-center items-start" id="project_hero">
                 <div class="flex flex-col mx-auto px-7 pt-8 w-full h-auto justify-left items-start">
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Solution</h1>
-                    <blockquote class="p-4 my-4 border-s-4 border-gray-300  dark:border-gray-500 dark:bg-gray-800">
-                        <p class="xxs:text-sm md:text-xl italic font-bold leading-relaxed">"Establish a source of truth for all staff information and integrate different systems through centralized integration hub"</p>
-                    </blockquote>
-                    <p class="my-4">
-                        The goal is to design and develop a scalable customized middleware system that allows the data flows from source of truth to all other systems.<br/><br/>
-                        Staff will make changes to one and only one system and the data will be replicated across all different systems that are connected to the middleware. <br/><br/>
-                        This ensures data consistency, reduce the workload and increase efficiency of the staff.<br/></p>
+                    <ul class="space-y-1 text-black list-disc list-inside dark:text-gray-400">
+                        <li>
+                            Firstly, we need to digitalize all paper forms to tackle the issue with storing physical papers in multiple folders for each machine.
+                        </li><br/>
+                        <span class="md:pl-5">To do that, We created a form with Microsoft solution called Microsoft forms. As the business is already in the process of rolling out individual tablet for each maintenance staff, the project was rolled out at the right time.<br/><br/></span>
+                        <li>
+                            Secondly, We need a place to store all those forms and manager to track of which machine has completed the process or which staff has not finished their tasks. Moreover, the manager also need to keep track of parts that are consumed as part of the maintenance process.
+                        </li><br/>
+                        <span class="md:pl-5">To do that, We created a user-friendly web portal with WordPress and linked directly with Microsoft Forms. As soon as the staff submit their forms, the manager is notified and can review those forms. The forms can be searched, sorted alphabetically and stored by the machine name.
+                            We also created a dedicated function for the business to upload the archived forms to the portal for the easier search but this was not included in the project due to the tight deadline.<br/><br/></span>
+                        <li>
+                            In addition, the supervisor can easily view the details of spare parts and equipments stock level in the system. Data to these details are fed through customized APIs.
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -117,19 +123,18 @@
                     <h1 class="font-scoville text-4xl" id="sectionHeader">Technologies Used</h1>
                     <ul class="max-w-lg space-y-1 text-black list-disc list-inside dark:text-gray-400">
                         <li>
-                            Vue for Frontend Components
+                            HTML, CSS for Frontend Components
                         </li>
                         <li>
-                            Laravel for all backend components
+                            WordPress (PHP) for all backend components
                         </li>
                         <li>
-                            Supported by MariaDB
+                            Supported by MySQL
                         </li>
                         <li>
-                            Hosted on AWS
+                            Hosted on Company's Server
                         </li>
                     </ul><br/>
-                    <p>Learn more about the whole process of the project and detailed technologies used in <a href="https://www.google.com" target="_blank" class="underline">Here</a>.</p>
                 </div>
             </div>
         </div>
