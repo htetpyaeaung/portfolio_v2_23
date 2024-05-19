fbf1c5a39bb5d072074702b442f1f36a40e656d5
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-05-19 21:00:51 +0930

    Added constructor, new sites info entry and post function in blog controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,15 +7,25 @@ use Dbfx\LaravelStrapi\LaravelStrapi;
 
 class BlogController extends Controller
 {
+    private $strapi;
+    public function __construct()
+    {
+        $this->strapi = new LaravelStrapi();
+    }
     public function blog()
     {
-        $strapi = new LaravelStrapi();
         $type = 'blogs';
-        $blogs= $strapi->collection($type,populate:['authors','cover']);
-
+        $blogs= $this->strapi->collection($type,populate:['authors','cover']);
         $blogs =  $blogs['data'];
-        // return $blogs;
-        
-        return view('blogs', compact(['blogs']));
+
+        $sitesInfo = $this->strapi->single('global');
+        $sitesInfo = $sitesInfo['data'];
+
+        return view('blogs', compact(['blogs'],['sitesInfo']));
+    }
+
+    public function post(int $postID)
+    {
+        return $blogs = $this->strapi->entry('blogs', $postID,populate:['authors']);
     }
 }
