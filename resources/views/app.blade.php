b7ef79db6251573801bf733314680d63a758425f
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2024-06-23 00:30:32 +0930

    Change heading fonts in landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,10 +36,6 @@
         </div>
         <!-- The overlay -->
         <div id="myNav" class="overlay">
-
-          <!-- Button to close the overlay navigation -->
-          {{-- <a href="javascript:void(0)" id="hamburger-close" class="closebtn" onclick="closeNav()">&times;</a> --}}
-
           <!-- Overlay content -->
           <div class="overlay-content">
             <a href="/about">About</a>
@@ -79,20 +75,20 @@
       </div>
     </div>
 
-    <div class="container absolute bottom-0 left-0">
-      <div class="container mx-auto flex flex-col space-y-2">
+    <div class="container xxs:w-1/3 absolute bottom-0 left-0 flex flex-col">
+      <div class="container space-y-4">
         <a href="https://github.com/htetpyaeaung" target="_blank" class="mb-1">
-          <svg class="hover:fill-white icons" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg">
+          <svg class="hover:fill-white icons mb-4" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg">
             <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>
           </svg>
         </a>
-        <a href="https://twitter.com" target="_blank">
-          <svg class="hover:fill-white icons" viewBox="0 0 1200 1227" xmlns="http://www.w3.org/2000/svg">
+        <a href="https://x.com/charles_htet" target="_blank">
+          <svg class="hover:fill-white icons mb-4" viewBox="0 0 1200 1227" xmlns="http://www.w3.org/2000/svg">
             <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"/>
           </svg>
         </a>
         <a href="https://www.linkedin.com/in/htetpyaeaung-charles/" target="_blank">
-          <svg class="hover:fill-white icons text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+          <svg class="hover:fill-white icons text-gray-800 dark:text-white mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
             <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
           </svg>
